208e5abaa50dd8a45b0669f89ad6b46e8580fc8f
saramsaramsaram <leezuhwan000@dgsw.hs.kr>
2026-04-02 17:50:14 +0900

    first commit

---
+++ src/main/java/com/saram/jellylog/JellyLogApplication.java
@@ -0,0 +1,13 @@
+package com.saram.jellylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JellyLogApplication.class, args);
+    }
+
+}
