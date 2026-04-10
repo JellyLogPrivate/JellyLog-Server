2f9ca0f354264d5294e98ff22d7be56ed08d185c
soyung0951 <dgsw0951@dgsw.hs.kr>
2026-04-10 23:29:14 +0900

    refactor: User 엔티티 수정 및 .DS_Store 정리

--- src/main/java/com/saram/jellylog/login/dto/LoginResponse.java
+++ src/main/java/com/saram/jellylog/login/dto/LoginResponse.java
@@ -2,9 +2,7 @@ package com.saram.jellylog.login.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
 @Getter
 @NoArgsConstructor
 public class LoginResponse {
