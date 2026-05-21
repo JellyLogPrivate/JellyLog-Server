0237d2e71c9d3fec00f41da2ed22a78319de1655
saramsaramsaram <leezuhwan000@dgsw.hs.kr>
2026-05-21 17:24:48 +0900

    chore : gemini api 모델 교체

--- src/main/java/com/saram/jellylog/question/service/QuestService.java
+++ src/main/java/com/saram/jellylog/question/service/QuestService.java
@@ -60,7 +60,7 @@ public class QuestService {
     }
 
     private Quest generateAndSaveQuest() {
-        String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + apiKey;
+        String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=" + apiKey;
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
